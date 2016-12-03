46e8d61bc000eaa8f019ed3e2979c588d5f0d31c
i4igo <petruxaxa@gmail.com>
2016-12-03 12:33:32 +0200

    clear

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -2,7 +2,6 @@ package com.itea.android.itea_l2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
