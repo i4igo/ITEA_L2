e9f050c72ae7751a9ea7a3ee8ac4b81031937a97
i4igo <petruxaxa@gmail.com>
2016-12-07 23:03:09 +0200

    add list

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -2,12 +2,36 @@ package com.itea.android.itea_l2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ListView lvListImage;
+    private LVAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lvListImage = (ListView) findViewById(R.id.lvListImage);
+        adapter = new LVAdapter(this, R.layout.activity_adapter);
+        lvListImage.setAdapter(adapter);
+
+        List<ImageView> l = new ArrayList<ImageView>();
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        l.add(new ImageView(getApplicationContext()));
+        adapter.updateList(l);
+
     }
 }
