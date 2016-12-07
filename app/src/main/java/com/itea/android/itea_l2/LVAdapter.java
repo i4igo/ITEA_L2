75b986cc3b42306a4d2152c14f14a91b157883c4
i4igo <petruxaxa@gmail.com>
2016-12-07 23:02:46 +0200

    custom adapter

---
+++ app/src/main/java/com/itea/android/itea_l2/LVAdapter.java
@@ -0,0 +1,68 @@
+package com.itea.android.itea_l2;
+
+import android.content.Context;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LVAdapter extends ArrayAdapter<ImageView> {
+
+    private Context context;
+    private List<ImageView> list;
+    private LayoutInflater inflater;
+
+    public LVAdapter(Context context, int resource) {
+        super(context, resource);
+        list = new ArrayList<>();
+        this.context = context;
+        inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    public List<ImageView> getList() {
+        return list;
+    }
+
+    @Nullable
+    @Override
+    public ImageView getItem(int position) {
+        return list.get(position);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (view == null)
+            view = inflater.inflate(R.layout.activity_adapter, parent, false);
+        ImageView ivImage = (ImageView) view.findViewById(R.id.ivImage);
+
+        Picasso.with(getContext())
+                .load("http://a1.mzstatic.com/us/r30/Purple3/v4/ed/dd/e3/eddde34d-892d-a1f7-b60c-1a515dad042d/icon256x256.jpeg")
+                .resize(64, 64)
+                .into(ivImage);
+
+        return view;
+    }
+
+    public void updateList(List<ImageView> list) {
+        this.list.clear();
+        this.list.addAll(list);
+        notifyDataSetChanged();
+        Toast.makeText(getContext(), "updateList", Toast.LENGTH_SHORT).show();
+    }
+}
