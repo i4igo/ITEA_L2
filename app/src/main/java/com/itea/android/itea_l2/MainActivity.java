1f6390f257ac25358e2b98a0763f2c5485b0161a
i4igo <petruxaxa@gmail.com>
2016-12-10 20:03:20 +0200

    verification input

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -25,15 +25,29 @@ public class MainActivity extends AppCompatActivity {
         bLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // проверка логина и пароля
 
-                Intent intent = new Intent(MainActivity.this, PersonActivity.class);
-                //intent.putExtra(Constants.KEY, etLogin.getText().toString());
-                startActivity(intent);
-                // передать логин и вывести его в тулбаре следующего окна
+                /**проверка логина и пароля
+                 *  передать логин и вывести его в тулбаре следующей активности
+                 *  открыть новую активность и закрыть эту
+                 * при ошибках ввода, заставить исправить их
+                 */
+                if (ValidInput.validLogin(etLogin.getText().toString())) {
+                    String login = etLogin.getText().toString();
 
-                // finish activity 1
-                finish();
+                    if (ValidInput.validPassword(etPassword.getText().toString())) {
+
+                        Intent intent = new Intent(MainActivity.this, PersonActivity.class);
+                        intent.putExtra(Constants.KEY, login);
+                        startActivity(intent);
+                        finish();
+                    } else {
+                        etPassword.requestFocus();
+                        etPassword.setError("Check your password");
+                    }
+                } else {
+                    etLogin.requestFocus();
+                    etLogin.setError("Check your email");
+                }
             }
         });
     }
