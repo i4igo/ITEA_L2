588dad28e0ace3b69c0b0c3f9a18521be8d019a1
i4igo <petruxaxa@gmail.com>
2016-12-10 17:34:38 +0200

    image from gallery

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -1,37 +1,56 @@
 package com.itea.android.itea_l2;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.ImageView;
-import android.widget.ListView;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ListView lvListImage;
-    private LVAdapter adapter;
+    private static final int PICK_IMAGE_REQUEST = 1122;
+    private ImageView ivFromGallery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        lvListImage = (ListView) findViewById(R.id.lvListImage);
-        adapter = new LVAdapter(this, R.layout.activity_adapter);
-        lvListImage.setAdapter(adapter);
+        ivFromGallery = (ImageView) findViewById(R.id.ivFromGallery);
 
-        List<ImageView> l = new ArrayList<ImageView>();
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        l.add(new ImageView(getApplicationContext()));
-        adapter.updateList(l);
+        ivFromGallery.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent();
+                intent.setType("image/*");
+                intent.setAction(Intent.ACTION_GET_CONTENT);
+                startActivityForResult(Intent.createChooser(intent, "Select Picture"), PICK_IMAGE_REQUEST);
+            }
+        });
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == PICK_IMAGE_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri uri = data.getData();
+
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                // Log.d(TAG, String.valueOf(bitmap));
+
+                ImageView imageView = (ImageView) findViewById(R.id.ivFromGallery);
+                imageView.setImageBitmap(bitmap);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
