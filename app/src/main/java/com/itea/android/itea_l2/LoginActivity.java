632740c2ab0bfe2cec272151914bc51bed34cc53
i4igo <petruxaxa@gmail.com>
2016-12-17 16:05:34 +0200

    SharedPref & checkBox

--- app/src/main/java/com/itea/android/itea_l2/LoginActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/LoginActivity.java
@@ -1,17 +1,22 @@
 package com.itea.android.itea_l2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 public class LoginActivity extends AppCompatActivity {
 
     private EditText etLogin;
     private EditText etPassword;
+    private CheckBox cbRemember;
     private Button bLogin;
+
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,6 +25,7 @@ public class LoginActivity extends AppCompatActivity {
 
         etLogin = (EditText) findViewById(R.id.etLogin);
         etPassword = (EditText) findViewById(R.id.etPassword);
+        cbRemember = (CheckBox) findViewById(R.id.cbRemember);
         bLogin = (Button) findViewById(R.id.bLogin);
 
         bLogin.setOnClickListener(new View.OnClickListener() {
@@ -31,24 +37,36 @@ public class LoginActivity extends AppCompatActivity {
                  *  открыть новую активность и закрыть эту
                  * при ошибках ввода, заставить исправить их
                  */
-                if (ValidInput.validLogin(etLogin.getText().toString())) {
-                    String login = etLogin.getText().toString();
 
-                    if (ValidInput.validPassword(etPassword.getText().toString())) {
+                int i = 0;
+                String login = "";
 
-                        Intent intent = new Intent(LoginActivity.this, PersonActivity.class);
-                        intent.putExtra(Constants.KEY, login);
-                        startActivity(intent);
-                        finish();
-
-                    } else {
-                        etPassword.requestFocus();
-                        etPassword.setError("Check your password");
-                    }
-                } else {
-                    etLogin.requestFocus();
-                    etLogin.setError("Check your email");
+                if (ValidInput.validLogin(etLogin)) {
+                    i++;
+                    login = etLogin.getText().toString();
                 }
+
+                if (ValidInput.validPassword(etPassword)) i++;
+                else if (login.length() != 0)
+                    etPassword.requestFocus();
+
+                sharedPref = getSharedPreferences(Constants.FILE_SAVE, MODE_PRIVATE);
+                SharedPreferences.Editor editor = sharedPref.edit();
+                if (cbRemember.isChecked()) {
+                    editor.putString(Constants.KEY_SAVE, login);
+                    editor.commit();
+                } else {
+                    editor.putString(Constants.KEY_SAVE, "not_remember");
+                    editor.commit();
+                }
+
+                if (i == 2) {
+                    Intent intent = new Intent(LoginActivity.this, PersonActivity.class);
+                    intent.putExtra(Constants.KEY, login);
+                    startActivity(intent);
+                    finish();
+                }
+
             }
         });
     }
