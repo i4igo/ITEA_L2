08e72a2d856c199c7745b696fd7290cbf75a1cd9
i4igo <petruxaxa@gmail.com>
2016-12-17 15:57:03 +0200

    add getSharedPreferences

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -1,27 +1,31 @@
 package com.itea.android.itea_l2;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText etLogin;
-    private EditText etPassword;
-    private Button bLogin;
+    private SharedPreferences sharedPref;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Intent intent = new Intent(MainActivity.this, LoginActivity.class);
-        startActivity(intent);
-        finish();
+        sharedPref = getSharedPreferences(Constants.FILE_SAVE, MODE_PRIVATE);
+        String answer = sharedPref.getString(Constants.KEY_SAVE, "");
+        if (!answer.equals("not_remember")) {
+            Intent intent = new Intent(MainActivity.this, PersonActivity.class);
+            intent.putExtra(Constants.KEY, answer);
+            startActivity(intent);
+            finish();
+        } else {
+            Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+            startActivity(intent);
+            finish();
+        }
 
     }
 }
