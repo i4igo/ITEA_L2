c78623ffcbf1516dc7641e4c2685deba0419a70e
i4igo <petruxaxa@gmail.com>
2016-12-21 23:47:49 +0200

    alert dialog tested

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -4,42 +4,69 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
 import android.widget.Toast;
 
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    private DialogInterface.OnMultiChoiceClickListener onMultiChoiceClickListener;
+    private boolean[] checkedItems;
+    private String[] listData;
+    private ArrayAdapter<String> ad;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        AlertDialog.Builder adBuilder = new AlertDialog.Builder(this);
-        adBuilder.setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+        ButterKnife.bind(this);
+
+        listData = new String[]{"Киев", "Львов", "Одесса", "Харьков"};
+        checkedItems = new boolean[]{false, true, false, false};
+        onMultiChoiceClickListener = new DialogInterface.OnMultiChoiceClickListener() {
             @Override
-            public void onClick(DialogInterface dialog, int which) {
-                Toast.makeText(MainActivity.this, "Positive Button", Toast.LENGTH_SHORT).show();
+            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
             }
-        })
+        };
+        ad = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+    }
 
-                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+    @OnClick(R.id.bAlertDialog)
+    public void bAlertDialogClick(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder
+                .setCancelable(true)
+                .setPositiveButton("Positive", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this, "Negative Button", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Positive button Clicked", Toast.LENGTH_SHORT).show();
                     }
                 })
-
-                .setNeutralButton("Maybe", new DialogInterface.OnClickListener() {
+                .setNegativeButton("Negative", new DialogInterface.OnClickListener() {
                     @Override
                     public void onClick(DialogInterface dialog, int which) {
-                        Toast.makeText(MainActivity.this, "Maybe Button", Toast.LENGTH_SHORT).show();
+                        Toast.makeText(MainActivity.this, "Negative button Clicked", Toast.LENGTH_SHORT).show();
                     }
                 })
-
-                //*AlertDialog alertDialog = adBuilder.create();
-                //alertDialog.show();
-                .setTitle("Test title")
-                .setIcon(R.drawable.person);
-
-        adBuilder.show();
-
+                .setNeutralButton("Neutral", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, "Neutral button Clicked", Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setIcon(R.drawable.error_dialog)
+                .setTitle("Test Alert dialog")
+//                .setMultiChoiceItems(listData, checkedItems, onMultiChoiceClickListener)
+                .setAdapter(ad, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Toast.makeText(MainActivity.this, String.valueOf(which), Toast.LENGTH_SHORT).show();
+                    }
+                })
+                .setView(R.layout.test_view_alert_dialog)
+//                .setMessage("My message")
+                .create().show();
     }
 }
