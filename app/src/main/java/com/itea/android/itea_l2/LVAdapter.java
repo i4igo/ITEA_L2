bc22cdd04925cd530caa7e3ccb9e3af7987d75f4
i4igo <petruxaxa@gmail.com>
2016-12-27 17:18:12 +0200

    add Picasso

--- app/src/main/java/com/itea/android/itea_l2/LVAdapter.java
+++ app/src/main/java/com/itea/android/itea_l2/LVAdapter.java
@@ -1,23 +1,23 @@
 package com.itea.android.itea_l2;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.provider.MediaStore;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 import java.util.List;
 
-import static com.itea.android.itea_l2.R.id.ivListItem;
 
 /**
  * Created by Ev on 06.12.2016.
@@ -35,7 +35,7 @@ public class LVAdapter extends ArrayAdapter<POJO> {
         inflater = (LayoutInflater) this.context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
     }
 
-    public List<POJO> getList(){
+    public List<POJO> getList() {
         return list;
     }
 
@@ -55,34 +55,38 @@ public class LVAdapter extends ArrayAdapter<POJO> {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
         View view = convertView;
-        if (view == null){
+        if (view == null) {
             view = inflater.inflate(R.layout.each_item_list, parent, false);
         }
 
-        //TextView tImage = (TextView) view.findViewById(R.id.ivListItem);
-        TextView tName = (TextView) view.findViewById(R.id.tName);
-        TextView tLName = (TextView) view.findViewById(R.id.tLName);
+        ImageView tImage = (ImageView) view.findViewById(R.id.ivIcon);
+        TextView tFullName = (TextView) view.findViewById(R.id.tName);
+        //TextView tLName = (TextView) view.findViewById(R.id.tLName);
         TextView tPNumber = (TextView) view.findViewById(R.id.tPNumber);
-        TextView tCountry = (TextView) view.findViewById(R.id.tCountry);
-        TextView tCity = (TextView) view.findViewById(R.id.tCity);
+        //TextView tCountry = (TextView) view.findViewById(R.id.tCountry);
+        //TextView tCity = (TextView) view.findViewById(R.id.tCity);
+        TextView tAdress = (TextView) view.findViewById(R.id.tCountry);
         TextView tEmail = (TextView) view.findViewById(R.id.tEmail);
         TextView tNotes = (TextView) view.findViewById(R.id.tNotes);
 
-        tName.setText(list.get(position).getEtName());
-        tLName.setText(list.get(position).getEtLName());
+        //Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), list.get(position).getIvImage());
+        //tImage.setImageURI(Uri.parse(list.get(position).getIvImage()));
+        tFullName.setText(list.get(position).getEtName() + " " + list.get(position).getEtLName());
+        //tLName.setText(list.get(position).getEtLName());
         tPNumber.setText(list.get(position).getEtPNumber());
-        tCountry.setText(list.get(position).getEtCountry());
-        tCity.setText(list.get(position).getEtCity());
+        tAdress.setText(list.get(position).getEtCountry() + ", " + list.get(position).getEtCity());
+        //tCountry.setText(list.get(position).getEtCountry());
+        //tCity.setText(list.get(position).getEtCity());
         tEmail.setText(list.get(position).getEtEmail());
 
 
-        /*Picasso.with(getContext())
-                .load("http://a1.mzstatic.com/us/r30/Purple3/v4/ed/dd/e3/eddde34d-892d-a1f7-b60c-1a515dad042d/icon256x256.jpeg")
-                .resize(64, 64)
-                .into(ivListItem);*/
+        Picasso.with(getContext())
+                //.load("http://a1.mzstatic.com/us/r30/Purple3/v4/ed/dd/e3/eddde34d-892d-a1f7-b60c-1a515dad042d/icon256x256.jpeg")
+                .load(Uri.parse(list.get(position).getIvImage()))
+                .resize(60, 60)
+                .into(tImage);
         return view;
     }
-
 
     public void updateList(List<POJO> l) {
         this.list.clear();
