a4a2326e33696cc9544ed8ed8ca4445d3ef471bc
i4igo <petruxaxa@gmail.com>
2017-01-10 11:56:56 +0200

    data

--- app/src/main/java/com/itea/android/itea_l2/UpdateMessage.java
+++ app/src/main/java/com/itea/android/itea_l2/UpdateMessage.java
@@ -2,6 +2,23 @@ package com.itea.android.itea_l2;
 
 /**
  * Created by Ev on 10.01.2017.
+ * класс служит типом передаваеммых данных
+ * от нашего AsyncTask в GUI
+ * через метод publishProgress() в метод onProgressUpdate()
+ *
+ * передаваемые данные это:
+ *  - что уже подобрано (набор символов)
+ *  - время, за которое подобран символ
  */
+
 public class UpdateMessage {
+    
+    private String onHackPassword;
+    private int time;
+
+    public UpdateMessage(String onHackPassword, int time) {
+        super();
+        this.onHackPassword = onHackPassword;
+        this.time = time;
+    }
 }
