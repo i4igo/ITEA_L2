f6594d0804fbd5fc4030c4aabd7313fc1a8cd743
i4igo <petruxaxa@gmail.com>
2017-01-10 11:16:21 +0200

    AsyncTask

--- app/src/main/java/com/itea/android/itea_l2/MainActivity.java
+++ app/src/main/java/com/itea/android/itea_l2/MainActivity.java
@@ -1,31 +1,28 @@
 package com.itea.android.itea_l2;
 
+import android.os.AsyncTask;
+import android.os.Process;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.RadioButton;
+import android.widget.RadioGroup;
 import android.widget.TextView;
-import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private EditText etInPassword;
-
+    private RadioGroup rgMode;
     private RadioButton rbHandler;
     private RadioButton rbAsyncTask;
-
     private Button bHack;
-
     private CheckBox cbRun;
-
     private ProgressBar pb;
-
     private TextView tvTime;
     private TextView tvPassword;
     private TextView tvInfo;
@@ -36,13 +33,75 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         etInPassword = (EditText) findViewById(R.id.etInPassword);
-        rbHandler;
-        rbAsyncTask;
-        bHack;
-        cbRun;
-        pb;
-        tvTime;
-        tvPassword;
-        tvInfo;
+        rgMode = (RadioGroup) findViewById(R.id.rgMode);
+        rbHandler = (RadioButton) findViewById(R.id.rbHandler);
+        rbAsyncTask = (RadioButton) findViewById(R.id.rbAsyncTask);
+        bHack = (Button) findViewById(R.id.bHack);
+        cbRun = (CheckBox) findViewById(R.id.cbRun);
+        pb = (ProgressBar) findViewById(R.id.pb);
+        tvTime = (TextView) findViewById(R.id.tvTime);
+        tvPassword = (TextView) findViewById(R.id.tvPassword);
+        tvInfo = (TextView) findViewById(R.id.tvInfo);
+    }
+
+    @Override
+    public void onClick(View v) {
+        String needToHack = etInPassword.getText().toString();
+
+        switch (rgMode.getCheckedRadioButtonId()) {
+            case R.id.rbHandler:
+
+
+                break;
+
+            case R.id.rbAsyncTask:
+
+
+                break;
+        }
+    }
+
+    class TestAsyncTask extends AsyncTask<String, UpdateMessage, Void> {
+
+        // подготавливаем данные
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        // создаем новый процесс
+        @Override
+        protected Void doInBackground(String... params) {
+
+            //передаем данные
+            publishProgress();
+
+            return null;
+        }
+
+        // получаем данные
+        @Override
+        protected void onProgressUpdate(UpdateMessage... values) {
+            super.onProgressUpdate(values);
+        }
+
+        // получаем результат работы
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+        }
+
+        // останавливаем обновление данных экрана
+        @Override
+        protected void onCancelled(Void aVoid) {
+            super.onCancelled(aVoid);
+        }
+
+        @Override
+        protected void onCancelled() {
+            super.onCancelled();
+        }
+
+
     }
 }
