9aef810eafb36fb4313da25ab89832e6f5675b31
i4igo <petruxaxa@gmail.com>
2017-01-10 16:21:06 +0200

    add const

---
+++ app/src/main/java/com/itea/android/itea_l2/Constants.java
@@ -0,0 +1,14 @@
+package com.itea.android.itea_l2;
+
+/**
+ * Created by Ev on 10.01.2017.
+ */
+
+public interface Constants {
+
+    int MESSAGE_HACK_END = 1;
+    int MESSAGE_IN_PROGRESS = 0;
+
+    String MESSAGE_TIME = "time";
+    String MESSAGE_PASSWORD = "password";
+}
